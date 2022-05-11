5ce721d31b0e7790ae05e58e4d7c4af4fc20f7ce
bernard chisumo <bchisumo74@gmail.com>
2022-05-11 16:18:17 +0100

    Tag Data Modelling Completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +20,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Admin::factory()->create();
-        \App\Models\Post::factory(30)->create();
-        \App\Models\Category::factory(30)->create();
+        User::factory(10)->create();
+        Admin::factory()->create();
+        Post::factory(30)->create();
+        Category::factory(30)->create();
+        Tag::factory(8)->create(); // Create 8 tags
 
+        foreach (Post::all() as $post){
+            $random_tags= Tag::all()->random(rand(2,5))->pluck('id')->toArray();
+            foreach ($random_tags as $tag){
+                DB::table('post_tag')->insert([
+                    'post_id' => $post->id,
+                    'tag_id'=> Tag::all()->random(1)[0]->id
+                ]);
+            }
+        }
+
+        foreach (Category::all() as $category){
+            $random_posts= Post::all()->random(rand(2,5))->pluck('id')->toArray();
+            foreach ($random_posts as $post){
+                DB::table('category_post')->insert([
+                    'category_id' => $category->id,
+                    'post_id'=> Post::all()->random(1)[0]->id
+                ]);
+            }
+        }
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
