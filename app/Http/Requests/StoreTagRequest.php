779cbb5999bebe3f83973e8ba6592b23dcde4fc5
bernard chisumo <bchisumo74@gmail.com>
2022-05-11 17:27:41 +0100

    StoreTagRequest Implemented

--- app/Http/Requests/StoreTagRequest.php
+++ app/Http/Requests/StoreTagRequest.php
@@ -13,7 +13,7 @@ class StoreTagRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,12 @@ class StoreTagRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+
+            return [
+                'title'         => 'required',
+                'slug'          => 'required',
+                'status'        => 'required',
+            ];
+
     }
 }
