63a129de0e48f3e8bad9a4ec1a5571594fe874cd
bernard chisumo <bchisumo74@gmail.com>
2022-05-11 15:47:26 +0100

    Category Data Modelling completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
         \App\Models\Admin::factory()->create();
         \App\Models\Post::factory(30)->create();
+        \App\Models\Category::factory(30)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
